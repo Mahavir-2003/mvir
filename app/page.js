b2408a445a1eb8d206caf0a9fe343fb5001714b3
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-07-19 18:36:08 +0530

    Update Redirection URL

--- app/page.js
+++ app/page.js
@@ -1,13 +1,25 @@
+"use client"
 import LandingPage from "@/components/Home/LandingPage";
 import Cursor from "@/components/cursor/Cursor";
 import IntroLoader from "@/components/loader/IntroLoader";
+import { useEffect } from "react";
+
+// Redirect user to another Website
+
+
+
 
 export default function Home() {
+
+  useEffect(() => {
+    window.location.href = "https://mhvr.framer.website/";
+  }, []);
+
   return (
     <div>
-      <Cursor />
+      {/* <Cursor />
       <IntroLoader />
-      <LandingPage />
+      <LandingPage /> */}
     </div>
   )
 }
