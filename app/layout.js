5018134436730b3c893ad6a3df60f54d5017af1e
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-08-04 10:31:37 +0530

    SEO optimization

--- app/layout.js
+++ app/layout.js
@@ -6,10 +6,16 @@ const inter = Inter({ subsets: ['latin'] })
 const syne = Syne({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Mahavir Patel | GDSC UVPCE Web Domain Lead | Full-Stack Developer & UI/UX Designer',
-  description: 'Mahavir Patel is a Full-Stack Developer, UI/UX Designer, and GDSC UVPCE Web Domain Lead from Mehsana, Gujrat, India. He is currently pursuing a B.Tech in Computer Engineering at Ganpat University. Explore his portfolio for innovative digital solutions.',
+  title: 'Mahavir Patel | Full-Stack Developer & UI/UX Designer',
+  description: 'Mahavir Patel is a Full-Stack Developer, IJI/UX Designer, and GDSC UVPCE Web Domain Lead, currently pursuing a B.Tech in Computer Engineering at Ganpat University. Explore his portfolio for innovative digital solutions.',
+  keywords: 'Mahavir patel, Mahavir, Full-Stack Developer, UI/UX Designer, Web Development, GDSC, Mehsana, India',
+  author: 'Mahavir Patel',
+  viewport: 'width=device-width, initial-scale=1',
+  'og:title': 'Mahavir Patel | Full-Stack Developer & UI/UX Designer',
+  'og:description': 'Mahavir Patel is a Full-Stack Developer, IJI/UX Designer, and GDSC UVPCE Web Domain Lead, currently pursuing a B.Tech in Computer Engineering at Ganpat University. Explore his portfolio for innovative digital solutions.',
+  'og:type': 'website',
+  'og:url': 'https://mhvr.framer.website/',
 }
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
